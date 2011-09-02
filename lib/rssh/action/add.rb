6ca5eddde35eb1e060a0b5f30fd78b30ae6390b9
Mike Wagner <mwagner@digitalinsites.com>
2011-09-02 06:09:39 -0500

    catch exceptions and report them nicely

--- lib/rssh/action/add.rb
+++ lib/rssh/action/add.rb
@@ -2,21 +2,22 @@ module RSSH
   module Action
     class Add
 
-      def initialize
-      end
-
       def invoke options = {}
         @options = options
-        entry   = RSSH::Action::Entry.new options
+        begin
+          entry   = RSSH::Action::Entry.new options
   
-        raise "Tag already exists"   if RSSH.config.has_tag? entry.tag
-        raise "Entry already exists" if RSSH.config.has_host? entry.host
+          raise "Tag already exists for '#{entry.tag}'"    if RSSH.config.has_tag? entry.tag
+          raise "Entry already exists for '#{entry.host}'" if RSSH.config.has_host? entry.host
 
-        RSSH.config << entry
-        RSSH.config.save
-        puts "Saved #{entry.host} #{'with tag ' + entry.tag if entry.tag}"
+          RSSH.config << entry
+          RSSH.config.save
+          puts "Saved #{entry.host} #{'with tag ' + entry.tag if entry.tag}"
+        rescue Exception => e
+          puts e.message
+          exit
+        end
       end
-
 
     end
   end

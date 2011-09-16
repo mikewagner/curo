cd91399b353c6e0422235a914a2c0464ff1770d1
Mike Wagner <mwagner@digitalinsites.com>
2011-09-16 08:48:40 -0500

    cleanup

--- lib/rssh/action.rb
+++ lib/rssh/action.rb
@@ -1,10 +1,11 @@
 module RSSH
   module Action
 
-    autoload :Entry,  'rssh/action/entry'
-    autoload :Add,    'rssh/action/add'
-    autoload :List,   'rssh/action/list'
-    autoload :Remove, 'rssh/action/remove'
+    autoload :Entry,      'rssh/action/entry'
+    autoload :Add,        'rssh/action/add'
+    autoload :List,       'rssh/action/list'
+    autoload :Remove,     'rssh/action/remove'
+    autoload :Connection, 'rssh/action/connection'
 
   end
 end

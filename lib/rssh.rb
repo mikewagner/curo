0f58ddae1d49301a99d6f4dceb149b5edd2b8c7b
Mike Wagner <mwagner@digitalinsites.com>
2011-09-21 14:47:33 -0500

    removed options object - moved to executable

--- lib/rssh.rb
+++ lib/rssh.rb
@@ -3,7 +3,6 @@ module RSSH
   autoload :Configuration, 'rssh/configuration'
   autoload :Connnection,   'rssh/connection'
   autoload :Entry,         'rssh/entry'
-  autoload :Options,       'rssh/options'
   autoload :Runner,        'rssh/runner'
 
 end

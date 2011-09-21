56fa831c22aa895f732dc6043504f10d214c1a5b
Mike Wagner <mwagner@digitalinsites.com>
2011-09-21 15:56:05 -0500

    fixed misspelling

--- lib/rssh.rb
+++ lib/rssh.rb
@@ -1,7 +1,7 @@
 module RSSH
 
   autoload :Configuration, 'rssh/configuration'
-  autoload :Connnection,   'rssh/connection'
+  autoload :Connection,    'rssh/connection'
   autoload :Entry,         'rssh/entry'
   autoload :Runner,        'rssh/runner'
 
